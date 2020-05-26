ff33b61159622fcabf70b86fc2a1171bf2636b3c
Anton <avvassil@uwaterloo.ca>
2020-05-26 16:43:06 -0400

    Added attribute to external function

--- include/seadsa/sea_dsa.h
+++ include/seadsa/sea_dsa.h
@@ -24,7 +24,7 @@ extern void sea_dsa_set_alloca(const void *p);
 // sea-dsa will collapse the argument's cell
 extern void sea_dsa_collapse(const void *p);
 // sea-dsa will return a fresh memory object
-extern void *sea_dsa_new();
+extern void *sea_dsa_new() __attribute__((malloc));
 // sea-dsa will mark the node pointed by p as a sequence node of size sz
 // The noded pointed by p cannot be already a sequence node and its
 // size must be less or equal than sz.
